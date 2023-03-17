8a23e60c051c5ec589255d1a17ab5c2268bd4426
Radwa <radwaashraf78@gmail.com>
2023-03-17 12:39:45 -0700

    r

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -12,7 +12,8 @@ int main(void)
 	for (num = 0; num <= 9; num++)
 	{
 		putchar((num % 10) + '0');
-		
+		if (num == 9)
+			continue;
 		
 		putchar(',');
 		putchar(' ');
@@ -22,3 +23,4 @@ int main(void)
 
 	return (0);
 }
+
