58d8c7f840819998ec3777b0b41542b3aa65ed24
Radwa <radwaashraf78@gmail.com>
2023-03-21 14:53:07 -0700

    m

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,13 +1,18 @@
+#include "main.h"
+/**
+ * main - Entry point
+ *
+ * Return: Alway 0 (Sucess)
+ */
+
 int main(void)
 {
-	char *sh = "Holberton";
+	char *h = "Holberton\n";
 
-	while (*sh)
+	while (*h)
 	{
-		_putchar(*sh);
-		sh++;
+		_putchar(*h);
+		h++;
 	}
-	_putchar('\n');
-
 	return (0);
 }
